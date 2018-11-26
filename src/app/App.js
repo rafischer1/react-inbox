a05cc423e667885d4c7a6f25ec90367137849ff8
Robert Fischer <artiefischer@gmail.com>
2018-11-26 08:41:25 -0700

    messagelist

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import Toolbar from './Toolbar/Toolbar'
 import Message from './Message/Message'
+import MessageList from './MessageList/MessageList'
 import Compose from './Compose/Compose'
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
   render() {
     return (
       <div className="App">
       <Toolbar />
       <Message />
+      <MessageList messages={this.messages}/>
       <Compose />
       </div>
     );
---
+++ src/app/MessageList/MessageList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MessageList = ({messages}) => {
+  console.log(messages)
+  return (
+    <div>
+    message list
+    </div>
+  )
+}
+
+export default MessageList
