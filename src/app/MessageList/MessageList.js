c147ae999e36277c8370e87c9d602f8f62eaa166
Robert Fischer <artiefischer@gmail.com>
2018-11-27 16:26:37 -0700

    unread messages in inbox

--- src/app/MessageList/MessageList.js
+++ src/app/MessageList/MessageList.js
@@ -14,37 +14,40 @@ const MessageList = ({messages, starCallback, readCallback}) => {
   }
   
   return messages.map((el, idx) => {
-
   let read = el.read ? "read" : "unread"
   let selected = el.selected ? "selected" : "unselected"
   let starred = el.starred ? "fa-star" : "fa-star-o"
 
   return (
-   <div className={`row message ${read} ${selected}`} key={idx}>
-      <div className="col-xs-1">
-        <div className="row">
-          <div className="col-xs-2">
-            <input id={el.id} type="checkbox" checked={el.selected} onChange={readFunction} />
-          </div>
-          <div className="col-xs-2">
-            <i id={el.id} className={`star fa ${starred}`} onClick={starFunction}>&nbsp;&nbsp;&nbsp;&nbsp;</i>
-          </div>
-        </div>
-      </div>
-      <div className="col-xs-11">
-        <a href="/" target="_blank">
-          <div >
-            &nbsp;&nbsp;
-           <Labels labels={el.labels}/>
-            &nbsp;&nbsp;
-            {el.subject}
-          </div>
-        </a>
-      </div>
-    </div>
+   renderFunction(read, selected, idx, el, readFunction, starred, starFunction)
   )
 })
  
+}
+
+const renderFunction = (read, selected, idx, el, readFunction, starred, starFunction) => {
+  return <div className={`row message ${read} ${selected}`} key={idx}>
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
+          <input id={el.id} type="checkbox" checked={el.selected} onChange={readFunction} />
+        </div>
+        <div className="col-xs-2">
+          <i id={el.id} className={`star fa ${starred}`} onClick={starFunction}>&nbsp;&nbsp;&nbsp;&nbsp;</i>
+        </div>
+      </div>
+    </div>
+    <div className="col-xs-11">
+      <a href="/" target="_blank">
+        <div>
+          &nbsp; &nbsp;
+          <Labels labels={el.labels} />
+          &nbsp; &nbsp;
+          {el.subject}
+        </div>
+      </a>
+    </div>
+  </div>
 }
 
 export default MessageList
