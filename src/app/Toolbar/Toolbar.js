595fe76af387c51b4160815ff428ee348358539e
Robert Fischer <artiefischer@gmail.com>
2018-11-29 11:48:39 -0700

    done

--- src/app/Toolbar/Toolbar.js
+++ src/app/Toolbar/Toolbar.js
@@ -31,51 +31,65 @@ export default class Toolbar extends React.Component {
   }
 
   
-  selectedCount = this.props.messages.filter(message => message.selected).length
+
 
 
   render() {
-    let allOrNone = "fa-check-o" ? "fa-check-square-o" : "fa-square-o"
+    let selectedCount = this.props.messages.filter(message => message.selected).length
+    let allOrNone
+    switch (selectedCount) {
+      case 0:
+        allOrNone = "fa-square-o"
+        break
+      case (this.props.messages.length):
+        allOrNone = "fa-check-square-o"
+        break
+      default:
+        allOrNone = "fa-minus-square-o"
+    }
+  
+    
     let totalUnread = this.props.messages.filter(message => !message.read).length
+  
     return (
       <div className="row toolbar">
         <div className="col-md-12">
           <p className="pull-right">
             <span className="badge badge">{totalUnread}</span>
-            unread messages
+            unread {totalUnread === 1 ? "message" : "messages"} 
            </p>
 
           <button type="button" className="btn btn-danger" onClick={this.openCompose}>
-            <i className="fa fa-plus"></i>
+            <i className="fa fa-plus" ></i>
           </button>
           
-          <button className="btn btn-default" onClick={this.selectAllAction}>
+          <button className={`btn btn-default`} onClick={this.selectAllAction}>
             <i className={`fa ${allOrNone}`}></i>
           </button>
 
-          <button className="btn btn-default" onClick={this.markAsRead}>
+          <button disabled={selectedCount === 0} className="btn btn-default" onClick={this.markAsRead}>
             Mark As Read
           </button>
 
-          <button className="btn btn-default" onClick={this.markAsUnread}>
+          <button disabled={selectedCount === 0} className="btn btn-default" onClick={this.markAsUnread}>
             Mark As Unread
           </button>
 
-          <select className="form-control label-select" onChange={this.applyLabelSelect}>
+          <select disabled={selectedCount === 0} className="form-control label-select" onChange={this.applyLabelSelect}>
             <option>Apply label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
             <option value="gschool">gschool</option>
           </select>
 
-          <select className="form-control label-select" onChange={this.removeLabelSelect}>
+          <select disabled={selectedCount === 0} className="form-control label-select" onChange={this.removeLabelSelect}>
             <option>Remove label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
             <option value="gschool">gschool</option>
           </select>
 
-          <button className="btn btn-default" onClick={this.deleteAction}>
+          <button disabled={selectedCount === 0} className="btn btn-default" onClick={this.deleteAction}>
             <i className="fa fa-trash-o"></i>
           </button>
         </div>
